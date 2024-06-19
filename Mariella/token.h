a89e19156454fa740442bfc9a388de258d2b1f47
Mariella Jeanne <150491048+mariellajeanne@users.noreply.github.com>
2024-06-19 13:37:52 +0800

    Fixed priorities of && and ||.

--- Mariella/token.h
+++ Mariella/token.h
@@ -65,8 +65,8 @@ int getPriority(char *operator, PriorityType type) {
 
     switch(*operator) {
 
-        case '&': return 2;
-        case '|': return 3;
+        case '|': return 2;
+        case '&': return 3;
         case '=': return 4;
         case '>': case '<': return 5;
         case '+': case '-': return 6;
